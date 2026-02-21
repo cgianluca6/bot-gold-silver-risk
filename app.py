9bca6d237fe7157a4be3b1b3003815a25b14ef44
cgianluca6 <cgianluca@bluewin.ch>
2026-02-21 23:46:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from functools import wraps
 # ==========================================
 app = Flask(__name__)
 TOKEN = os.environ.get('TELEGRAM_TOKEN')
-MY_CHAT_ID = "TON_ID_NUMERIQUE"  # <--- METS TON ID ICI
+MY_CHAT_ID = "929066398"  # <--- METS TON ID ICI
 
 bot = telebot.TeleBot(TOKEN)
 
